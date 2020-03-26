dd32ad055bd33d76be5ebd36a64e7c48bedc5491
Resume Website <jdcollins242@gmail.com>
2020-03-26 19:37:02 -0400

    Updates

---
+++ precache-manifest.d19964dc5bda72139758eeeb9b327340.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d671edddc84dfe505c7d27d8ce5b7a",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "ea4abfc2661e8f485874",
+    "url": "/FaceRecognition/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "8861fcfd1e35ab364f91",
+    "url": "/FaceRecognition/static/css/main.b2536625.chunk.css"
+  },
+  {
+    "revision": "ea4abfc2661e8f485874",
+    "url": "/FaceRecognition/static/js/2.b8f87f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FaceRecognition/static/js/2.b8f87f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8861fcfd1e35ab364f91",
+    "url": "/FaceRecognition/static/js/main.85409075.chunk.js"
+  },
+  {
+    "revision": "744b63f99bd6ba07c42c",
+    "url": "/FaceRecognition/static/js/runtime-main.85662a52.js"
+  },
+  {
+    "revision": "584371a13f33dcafd09ef879d2d53393",
+    "url": "/FaceRecognition/static/media/brain.584371a1.png"
+  }
+]);
